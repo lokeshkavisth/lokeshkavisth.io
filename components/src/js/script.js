9d7f4fdd0d85bdfb9e89f6f6ebc7336d6c00e877
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2022-11-27 17:28:36 +0530

    nav fix

--- components/src/js/script.js
+++ components/src/js/script.js
@@ -1,33 +1,16 @@
 $(document).ready(function () {
-
     $(".menu__icon").click(function () {
-
         $(".menu__icon").toggleClass("menuX__icon");
         $(".nav__links").toggleClass("menu__toggle");
-
     });
+});
 
+const nav = document.querySelector("[data-navbar]");
 
-
-
-
-
-    const nav = document.querySelector("[data-navbar]");
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY >= 100) {
-            nav.classList.add("active__nav");
-        } else {
-            nav.classList.remove("active__nav");
-        }
-    });
-
-
-
-
-
-
-
-
-
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 99) {
+        nav.classList.add("active__nav");
+    } else {
+        nav.classList.remove("active__nav");
+    }
 });
