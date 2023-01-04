a8277354b80fef86af64654a5ce8473f0c1ee156
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2023-01-04 18:50:49 +0530

    content updated

--- components/src/js/script.js
+++ components/src/js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
-    $(".menu__icon").click(function () {
-        $(".menu__icon").toggleClass("menuX__icon");
-        $(".nav__links").toggleClass("menu__toggle");
-    });
+  $(".menu__icon").click(function () {
+    $(".menu__icon").toggleClass("menuX__icon");
+    $(".nav__links").toggleClass("menu__toggle");
+  });
 });
+
+function linkClicked() {
+  console.log("hello");
+  $(".menu__icon").removeClass("menuX__icon");
+  $(".nav__links").removeClass("menu__toggle");
+}
 
 const nav = document.querySelector("[data-navbar]");
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY >= 99) {
-        nav.classList.add("active__nav");
-    } else {
-        nav.classList.remove("active__nav");
-    }
+  if (window.scrollY >= 99) {
+    nav.classList.add("active__nav");
+  } else {
+    nav.classList.remove("active__nav");
+  }
 });
